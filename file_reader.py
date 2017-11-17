80f8037b45beedbf21ca1ebcf60f75261043bed7
TomokiMatsuno <tomoki.matsuno256@gmail.com>
2017-11-17 18:14:23 +0900

    test BI_POS pos accuracy

---
+++ file_reader.py
@@ -0,0 +1,262 @@
+import codecs
+import re
+
+def make_line_chunks(lines, trigger, end_marker=None):
+    lines_idx = 0
+    line_chuncks = []
+    pattern = trigger
+    repattern = re.compile(pattern)
+
+    while not repattern.match(lines[lines_idx][0]):
+        lines_idx += 1
+
+    while lines_idx < len(lines) and lines[lines_idx][0]:
+        if lines[lines_idx][0] == end_marker:
+            break
+
+        empty_list = []
+        if repattern.match(lines[lines_idx][0]):
+            line_chuncks.append(empty_list)
+            line_chuncks[-1].append(lines[lines_idx])
+            lines_idx += 1
+            continue
+        line_chuncks[-1].append(lines[lines_idx])
+        lines_idx += 1
+
+    return line_chuncks
+
+
+class DataFrame:
+    def __init__(self):
+        return
+
+    def file2lines(self, input_file, delimiter, encoding='euc-jp'):
+        with codecs.open(input_file, "r", encoding) as f:
+            data = f.read()
+            f.close()
+
+        lines = data.split('\n')
+
+        sents = []
+        for l in lines:
+            tokens = l.split(delimiter)
+            sents.append(tokens)
+        return sents
+
+    def line2tokens(self, line, indices=None):
+        tokens = []
+
+        if indices is None:
+            for idx in range(len(line)):
+                tokens.append(tokens[idx])
+            return
+
+        else:
+            for idx in indices:
+                tokens.append(line[idx])
+            return tokens
+
+
+class Char:
+    def __init__(self, form, bipos, chunk_bi):
+        self.form = form
+        self.word_bipos = bipos
+        self.chunk_bi = chunk_bi
+
+
+class Word:
+    def __init__(self, line, chunk_bi):
+        self.feats = []
+        self.chars = []
+        for token in line:
+            self.feats.append(token)
+
+        for i in range(len(line[0])):
+            if i == 0:
+                bi = 'B'
+            else:
+                bi = 'I'
+            bipos = bi + '_' + line[3]
+            self.chars.append(Char(line[0][i], bipos, chunk_bi))
+
+
+class Chunk:
+    def __init__(self, lines):
+        self.id = lines[0][1]
+        self.head = int(lines[0][2][0:-1])
+        self.rel = lines[0][2][-1]
+        self.words = []
+        chunk_bi = 'B'
+        for l in lines[1:]:
+            self.add_word(l, chunk_bi)
+            chunk_bi = 'I'
+
+    def add_word(self, line, chunk_bi):
+        self.words.append(Word(line, chunk_bi))
+
+
+class Sent:
+    def __init__(self, lines):
+        self.id = lines[0][1]
+        self.chunks = []
+        self.word_forms = []
+        self.char_forms = []
+        self.word_biposes = []
+        self.chunk_bis = []
+
+        self.make_chunks(lines)
+        for ch in self.chunks:
+            for word in ch.words:
+                self.word_forms.append(word.feats[0])
+                for char in word.chars:
+                    self.char_forms.append(char.form)
+                    self.word_biposes.append(char.word_bipos)
+                    self.chunk_bis.append(char.chunk_bi)
+
+
+
+    def make_chunks(self, lines):
+        chunk_lines = make_line_chunks(lines, '\*', 'EOS')
+
+        for cls in chunk_lines:
+            self.chunks.append(Chunk(cls))
+
+class Dict:
+    def __init__(self, doc, initial_entries=None):
+        self.i2x = {}
+        self.x2i = {}
+        self.freezed = False
+
+        if initial_entries is not None:
+            for ent in initial_entries:
+                self.add_entry(ent)
+
+        for line in doc:
+            for ent in line:
+                self.add_entry(ent)
+
+    def add_entry(self, ent):
+        if ent not in self.x2i:
+            if not self.freezed:
+                self.i2x[len(self.i2x)] = ent
+                self.x2i[ent] = len(self.x2i)
+            else:
+                self.x2i[ent] = self.x2i["NULL"]
+
+    def freeze(self):
+        self.freezed = True
+
+
+class DataFrameUD(DataFrame):
+    def __init__(self, lines):
+        self.sents = self.lines2sents(lines)
+
+    def lines2sents(self, lines):
+        ret = []
+
+        sent_lines = make_line_chunks(lines, '#', '\n')
+
+        sent_lines = make_line_chunks(lines, '#', '\n')
+
+
+
+        for sl in sent_lines:
+            tmp = [sl[0]]
+            for l in sl[1:]:
+                tmp.append(self.line2tokens(self, l, [1, 3, 6, 7]))
+        ret.append(tmp)
+
+        return ret
+
+    def sents2dicts(self, sents, initial_entries=None):
+        doc_word_forms = []
+        doc_biupos_forms = []
+        doc_head = []
+        doc_rel = []
+
+        for sent in sents:
+            B = True
+            for s in sent[1:]:
+                doc_word_forms.append(s[0])
+                if B:
+                    doc_biupos_forms.append("B_" + s[1])
+                else:
+                    doc_biupos_forms.append("I_" + s[1])
+                doc_head.append(s[2])
+                doc_rel.append(s[3])
+                B = False
+        if initial_entries is None:
+            initial_entries = ["NULL", "UNK", "ROOT"]
+        wd = Dict(doc_word_forms, initial_entries)
+        bupd = Dict(doc_biupos_forms, initial_entries)
+        rd = Dict(doc_rel, ["NULL"])
+
+        return wd, bupd, rd
+
+
+
+
+class DataFrameKtc(DataFrame):
+    def __init__(self, lines):
+        self.sents = self.lines2sents(lines)
+
+    def lines2sents(self, lines):
+        ret = []
+        sent_lines = make_line_chunks(lines, '#')
+
+        for sl in sent_lines:
+            ret.append(Sent(sl))
+
+        return ret
+
+
+    def sents2dicts(self, sents, initial_entries=None):
+        doc_word_forms = []
+        doc_char_forms = []
+        doc_word_biposes = []
+        doc_chunk_bis = []
+
+        for sent in sents:
+            doc_word_forms.append(sent.word_forms)
+            doc_char_forms.append(sent.char_forms)
+            doc_word_biposes.append(sent.word_biposes)
+            doc_chunk_bis.append(sent.chunk_bis)
+        if initial_entries is None:
+            initial_entries = ["NULL", "UNK", "ROOT"]
+        wd = Dict(doc_word_forms, initial_entries)
+        cd = Dict(doc_char_forms, initial_entries)
+        bpd = Dict(doc_word_biposes, ["NULL"])
+
+        return wd, cd, bpd
+
+
+    def sents2ids(dicts, sents):
+        wd = dicts[0]
+        cd = dicts[1]
+        bpd = dicts[2]
+        word_seqs = []
+        char_seqs = []
+        word_bipos_seqs = []
+        chunk_bi_seqs = []
+
+        for sent in sents:
+            tmp_word = []
+            tmp_char = []
+            tmp_word_bipos = []
+            tmp_chunk_bi = []
+            for wf in sent.word_forms:
+                tmp_word.append(wd.x2i[wf])
+            for cf in sent.char_forms:
+                tmp_char.append(cd.x2i[cf])
+            for bp in sent.word_biposes:
+                tmp_word_bipos.append(bpd.x2i[bp])
+            for cbi in sent.chunk_bis:
+                tmp_chunk_bi.append(0 if cbi == 'B' else 1)
+
+            word_seqs.append(tmp_word)
+            char_seqs.append(tmp_char)
+            word_bipos_seqs.append(tmp_word_bipos)
+            chunk_bi_seqs.append(tmp_chunk_bi)
+
+        return word_seqs, char_seqs, word_bipos_seqs, chunk_bi_seqs
+
---
+++ main.py
@@ -0,0 +1,269 @@
+import numpy as np
+import dynet as dy
+import glob
+
+from collections import defaultdict
+from read_file import *
+from paths import *
+from file_reader import DataFrameKtc
+from file_reader import DataFrameUD
+
+
+files = glob.glob(path2KTC + 'syn/*ED.*')
+#files = [path2KTC + 'syn/9501ED.KNP']
+
+print(path2UD)
+
+print(files)
+
+df = DataFrameKtc
+
+train_sents = []
+for file in files[0:-1]:
+    print('[train] reading this file: ', file)
+    lines = df.file2lines(df, file, ' ', 'euc-jp')
+    train_sents.extend(df.lines2sents(df, lines))
+wd, cd, bpd = df.sents2dicts(df, train_sents)
+
+wd.freeze()
+cd.freeze()
+bpd.freeze()
+
+dev_sents = []
+for file in files[-1:]:
+    print('[dev] reading this file: ', file)
+    lines = df.file2lines(df, file, ' ', 'euc-jp')
+    dev_sents.extend(df.lines2sents(df, lines))
+
+for sent in dev_sents:
+    for w in sent.word_forms:
+        wd.add_entry(w)
+    for c in sent.char_forms:
+        cd.add_entry(c)
+    for bp in sent.word_biposes:
+        bpd.add_entry(bp)
+
+
+
+train_word_seqs, train_char_seqs, train_word_bipos_seqs, train_chunk_bi_seqs = df.sents2ids([wd, cd, bpd], train_sents)
+
+TRAIN = True
+epoc = 100
+train_iter = 1
+batch_size = 10
+show_loss_every = 10
+show_acc_every = 10
+
+LAYERS = 1
+HIDDEN_DIM = 200
+INPUT_DIM = HIDDEN_DIM * 2
+
+dev_word_seqs, dev_char_seqs, dev_word_bipos_seqs, dev_chunk_bi_seqs = df.sents2ids([wd, cd, bpd], dev_sents)
+
+###Neural Network
+
+VOCAB_SIZE = len(cd.i2x) + 1
+BIPOS_SIZE = len(bpd.i2x) + 1
+
+pc = dy.ParameterCollection()
+
+l2rlstm = dy.LSTMBuilder(LAYERS, INPUT_DIM, HIDDEN_DIM, pc)
+r2llstm = dy.LSTMBuilder(LAYERS, INPUT_DIM, HIDDEN_DIM, pc)
+
+params = {}
+params["lp_c"] = pc.add_lookup_parameters((VOCAB_SIZE, INPUT_DIM))
+params["lp_bp"] = pc.add_lookup_parameters((VOCAB_SIZE, INPUT_DIM))
+
+params["R"] = pc.add_parameters((BIPOS_SIZE, HIDDEN_DIM * 2))
+params["bias"] = pc.add_parameters((BIPOS_SIZE))
+
+params["R_bi_b"] = pc.add_parameters((2, INPUT_DIM * 2))
+params["bias_bi_b"] = pc.add_parameters((2))
+
+
+def linear_interpolation(bias, R, inputs):
+    ret = bias
+    for i in range(len(inputs)):
+        ret += R * inputs[i]
+    return ret
+
+
+
+def do_one_sentence(l2rlstm, r2llstm, char_seq, bipos_seq):
+    num_cor = 0
+    num_cor_bi = 0
+    num_cor_pos = 0
+
+    num_b = 0
+
+    dy.renew_cg()
+    s_l2r_0 = l2rlstm.initial_state()
+    s_r2l_0 = r2llstm.initial_state()
+
+    R = dy.parameter(params["R"])
+    bias = dy.parameter(params["bias"])
+    lp_c = params["lp_c"]
+
+    s_l2r = s_l2r_0
+    s_r2l = s_r2l_0
+
+    loss = []
+
+    cembs = [lp_c[c] for c in char_seq]
+
+    l2r_outs = s_l2r.add_inputs(cembs)
+    r2l_outs = s_r2l.add_inputs(reversed(cembs))
+    lstm_outs = [dy.concatenate([l2r_outs[i].output(), r2l_outs[i].output()]) for i in range(len(l2r_outs))]
+
+    for i in range(len(char_seq)):
+        probs = dy.softmax(R*lstm_outs[i] + bias)
+        loss.append(-dy.log(dy.pick(probs, bipos_seq[i])))
+
+        if(not TRAIN):
+            chosen = np.argmax(probs.npvalue())
+            #print(bpd.i2x[chosen], " ", bpd.i2x[bipos_seq[i]])
+            if(chosen == bipos_seq[i]):
+                num_cor += 1
+            if((bpd.i2x[chosen])[0] == (bpd.i2x[bipos_seq[i]])[0]):
+                num_cor_bi += 1
+            # if ((bpd.i2x[chosen])[1:-1] == (bpd.i2x[bipos_seq[i]])[1:-1]):
+            #     num_cor_pos += 1
+            if (bpd.i2x[chosen])[0] == 'B':
+                num_b += 1
+                if ((bpd.i2x[chosen])[1:-1] == (bpd.i2x[bipos_seq[i]])[1:-1]):
+                    num_cor_pos += 1
+
+    loss = dy.esum(loss)
+    return loss, num_cor, num_cor_bi, num_cor_pos, num_b
+
+
+
+    loss = dy.esum(loss)
+    return loss, num_cor
+
+def get_wh_seq(char_seq, bipos_seq, bi_b_seq):
+    ret = []
+    w_1st_chars = []
+
+    tmp = []
+    lp_c = params["lp_c"]
+    lp_bp = params["lp_bp"]
+
+    i = 0
+    tmp.append(dy.concatenate([lp_c[char_seq[i]], lp_bp[bipos_seq[i]]]))
+    w_1st_chars.append(i)
+    i += 1
+    while i < len(char_seq):
+        if bpd.i2x[bipos_seq[i]][0] != "B":
+            tmp.append(dy.concatenate([lp_c[char_seq[i]], lp_bp[bipos_seq[i]]]))
+            i += 1
+        else:
+            ret.append(tmp)
+            tmp.clear()
+            tmp.append(dy.concatenate([lp_c[char_seq[i]], lp_bp[bipos_seq[i]]]))
+            w_1st_chars.append(i)
+            i += 1
+    return ret, w_1st_chars
+
+def bi_b(wh_seq, w_1st_chars, bi_b_seq):
+    num_cor = 0
+
+    R_bi_b = dy.parameter(params["R_bi_b"])
+    bias_bi_b = dy.parameter(params["bias_bi_b"])
+    loss = []
+    for i in range(len(wh_seq)):
+        probs = dy.softmax(linear_interpolation(bias_bi_b, R_bi_b, wh_seq[i]))
+        loss.append(-dy.log(dy.pick(probs, bi_b_seq[w_1st_chars[i]])))
+
+        if not TRAIN:
+            chosen = np.asscalar(np.argmax(probs.npvalue()))
+            if(chosen == bi_b_seq[w_1st_chars[i]]):
+                # print(chosen, " ", bi_b_seq[w_1st_chars[i]])
+                num_cor += 1
+    loss = dy.esum(loss)
+    return loss, num_cor
+
+
+
+def train(l2rlstm, r2llstm, char_seqs, bipos_seqs, bi_b_seqs):
+    trainer = dy.SimpleSGDTrainer(pc)
+    losses = []
+    for it in range(train_iter):
+        for i in (range(len(char_seqs))):
+            idx = i if not TRAIN else np.random.randint(len(char_seqs))
+            if len(char_seqs[idx]) == 0 or len(bi_b_seqs[idx]) == 0:
+                continue
+            loss, _, _, _, _ = do_one_sentence(l2rlstm, r2llstm, char_seqs[idx], bipos_seqs[idx])
+            losses.append(loss)
+            if(i % batch_size):
+                loss_value = loss.value()
+                loss.backward()
+                trainer.update()
+            if i % show_loss_every == 0 and i != 0:
+                print(i, " bipos loss")
+                print(loss_value)
+
+            seq_wh, w_1st_chars = get_wh_seq(char_seqs[i], bipos_seqs[i], bi_b_seqs[i])
+            if(len(seq_wh) == 0):
+                continue
+            loss_bi_b, _ = bi_b(seq_wh, w_1st_chars, bi_b_seqs[i])
+
+            if i % batch_size:
+                loss_bi_b_value = loss_bi_b.value()
+                loss_bi_b.backward()
+                trainer.update()
+            if i % show_loss_every == 0 and i != 0:
+                print(i, " bi_b loss")
+                print(loss_bi_b_value)
+
+
+
+
+
+def dev(l2rlstm, r2llstm, char_seqs, bipos_seqs, bi_b_seqs):
+    num_tot = 0
+    num_tot_cor = 0
+
+    num_tot_cor_bi = 0
+    num_tot_cor_pos = 0
+
+    num_tot_bi_b = 0
+    num_tot_cor_bi_b = 0
+
+    num_tot_b = 0
+    for i in range(len(char_seqs)):
+        if(len(char_seqs[i]) == 0):
+            continue
+        loss, num_cor, num_cor_bi, num_cor_pos, num_b = do_one_sentence(l2rlstm, r2llstm, char_seqs[i], bipos_seqs[i])
+        num_tot += len(char_seqs[i])
+        num_tot_cor += num_cor
+        num_tot_cor_bi += num_cor_bi
+        num_tot_cor_pos += num_cor_pos
+
+        num_tot_b += num_b
+
+        if i % show_acc_every == 0 and i != 0:
+            print("accuracy bipos: ", num_tot_cor / num_tot)
+            print("accuracy bi: ", num_tot_cor_bi / num_tot)
+            # print("accuracy pos: ", num_tot_cor_pos / num_tot)
+            print("accuracy pos: ", num_tot_cor_pos / num_tot_b)
+            print("loss bipos: ", loss.value())
+
+        seq_wh, w_1st_chars = get_wh_seq(char_seqs[i], bipos_seqs[i], bi_b_seqs[i])
+        if (len(seq_wh) == 0):
+            continue
+        loss_bi_b, num_cor_bi_b = bi_b(seq_wh, w_1st_chars, bi_b_seqs[i])
+        num_tot_bi_b += len(seq_wh)
+        num_tot_cor_bi_b += num_cor_bi_b
+        if i % show_acc_every == 0 and i != 0:
+            print("accuracy chunking: ", num_tot_cor_bi_b / num_tot_bi_b)
+            print("loss chuncking: ", loss_bi_b.value())
+
+    return
+
+
+for e in range(epoc):
+    TRAIN = True
+    train(l2rlstm, r2llstm, train_char_seqs, train_word_bipos_seqs, train_chunk_bi_seqs)
+    TRAIN = False
+    dev(l2rlstm, r2llstm, dev_char_seqs, dev_word_bipos_seqs, dev_chunk_bi_seqs)
